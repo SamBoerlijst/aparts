79948444239010b668196451148bfc99db937f43
Sam Boerlijst <34442338+SamBoerlijst@users.noreply.github.com>
2023-05-05 03:09:55 +0200

    Add files via upload

--- doc/scholar_record_extraction.py
+++ doc/scholar_record_extraction.py
@@ -6,12 +6,32 @@ pg = ProxyGenerator()
 pg.FreeProxies()
 scholarly.use_proxy(pg)
 
-def download_articles(search_query: str, number: int = 100, year_range: list = [2000, 2020], start_index: int = 0):
+def download_articles(search_query: str, number: int = 100, year_range: list = [2000, 2020]) -> None:
+    """
+    Download article metadata to a csv based on a query. The function allows for intterruption at any time so that it is not impacted if the Google Scholar server stops responding due to the number of queries. In this case, just run the function again.
+
+    Parameters:
+    -----------
+    search_query (str): Query representative of field of research to search articles for.
+    number (int): Number of articles to query and store.
+    year_range (list): List stating the time period of articles to include.
+
+    Returns:
+    -----------
+    None 
+    """
     year_low = year_range[0]
     year_high = year_range[1]
+    
+    if os.path.exists('articles.csv'):
+        with open('articles.csv', mode='r', encoding='utf-8') as file:
+            rows = csv.reader(file)
+            start_index = sum(1 for row in rows) - 1  # subtract 1 to exclude the header row
+
+
     search_results = scholarly.search_pubs(query=search_query, year_low=year_low, year_high=year_high, citations=False, start_index=start_index)
 
-    headers = ['title', 'authors', 'year', 'abstract', 'url']
+    headers = ['title', 'authors', 'year', 'abstract', 'journal', 'url']
     file_exists = os.path.isfile('articles.csv')
 
     with open('articles.csv', mode='a', encoding='utf-8', newline='') as file:
@@ -20,15 +40,18 @@ def download_articles(search_query: str, number: int = 100, year_range: list = [
             writer.writerow(headers)
 
         for i, pub in enumerate(search_results):
-            if i < number:
+            if i < (number+start_index) :
+                pub = scholarly.search_single_pub(pub['bib']['title'], filled=True)
                 pub = scholarly.fill(pub)
+                print(pub)
                 title = pub['bib']['title']
                 authors = pub['bib']['author']
                 pub_year = pub['bib']['pub_year']
+                journal = pub['bib']['journal']
                 abstract = pub['bib']['abstract'].replace('\n', ' ')
                 pub_url = pub['pub_url']
-                writer.writerow([title, authors, pub_year, abstract, pub_url])
-                print(f'Article {i+1+start_index}/{number} downloaded and exported to CSV.')
+                writer.writerow([title, authors, pub_year, abstract, journal, pub_url])
+                print(f'Article {i+start_index}/{number} downloaded and exported to CSV.')
             else:
                 break
 
