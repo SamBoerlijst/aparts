31c9974ad35ea803df0a396dafca87eacfc78649
Sam Boerlijst <34442338+SamBoerlijst@users.noreply.github.com>
2023-05-09 20:23:51 +0200

    Add files via upload

--- setup.py
+++ setup.py
@@ -1,23 +1,24 @@
-from setuptools import find_packages, setup
+from setuptools import find_packages
+from distutils.core import setup
 
-with open("app/Readme.md", "r") as f:
+with open("Readme.md", "r") as f:
     long_description = f.read()
 
 setup(
-    name="AP-ART",
+    name="aparts",
     version="0.0.1",
     description="Tags pdf files using a tailored list of keywords and stores the results in a bib file and markdown summaries to be used in reference managers and personal knowledge management systems. ",
     package_dir={"": "app"},
     packages=find_packages(where="app"),
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/SamBoerlijst/AP-ART",
+    url="https://github.com/SamBoerlijst/aparts",
     author="Sam Boerlijst",
     author_email="samboerlijst@yahoo.com",
     license="MIT",
     classifiers=[
         "License :: OSI Approved :: MIT License",
-        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.9",
         "Operating System :: OS Independent",
     ],
     install_requires=["Levenshtein >= 0.20.9","pandas >= 1.5.2", "anyascii >= 0.3.1", "pybtex >= 0.24.0", "PyPDF2 >= 3.0.0", "gensim >= 4.3.0", "numpy >= 1.24.0", 
