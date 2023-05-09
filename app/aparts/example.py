5d7f0a50281fdc7c0a9bf44db43e2bf53a39e512
Sam Boerlijst <34442338+SamBoerlijst@users.noreply.github.com>
2023-05-10 00:13:58 +0200

    Add files via upload

---
+++ app/aparts/example.py
@@ -0,0 +1,13 @@
+from ..src.construct_keylist import generate_folder_structure, generate_keylist
+from ..src.APT import automated_pdf_tagging
+
+#generate folder structure for output
+generate_folder_structure()
+
+#generate keylist using a csv containing article records, the "Author Keywords" column therein and .bib file. 
+#generate_keylist(records = "input/records.csv", bibfile = "input/library.bib", author_given_keywords="Author Keywords")
+
+## It is advised to review the keylist document for artifacts before proceeding to the next step.
+
+#Tag articles using weighted tagging using a pdf folder and bib file.
+#automated_pdf_tagging(source_folder="C:/Users/sboer/Zotero/storage", bibfile="input/library.bib", alternate_lists="all", weighted = True, treshold = 5, summaries = True)
