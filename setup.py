49041886efaed4ca969c58dbdfa0b6cabef571db
Sam Boerlijst <34442338+SamBoerlijst@users.noreply.github.com>
2023-05-31 21:12:39 +0200

    Update setup.py
    Update dependencies to allow for pyvis node-network visualization.

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(
         "Programming Language :: Python :: 3.9",
         "Operating System :: OS Independent",
     ],
-    install_requires=["anyascii>=0.3.2", "cleantext>=1.1.4", "fuzzywuzzy>=0.18.0", "gensim>=4.3.1", "keybert>=0.7.0", "nlp_rake>=0.0.2", "nltk>=3.8.1", "numpy>=1.24.3", "pandas>=2.0.1", "pybtex>=0.24.0", "PyPDF2>=3.0.1", "python_Levenshtein>=0.21.0", "scholarly>=1.7.11", "scidownl>=1.0.2", "setuptools>=67.7.2", "six>=1.16.0", "spacy>=3.2.0", "yake>=0.4.8"],
+    install_requires=["anyascii>=0.3.2", "cleantext>=1.1.4", "fuzzywuzzy>=0.18.0", "gensim>=4.3.1", "keybert>=0.7.0", "nlp_rake>=0.0.2", "nltk>=3.8.1", "numpy>=1.24.3", "pandas>=2.0.1", "pybtex>=0.24.0", "PyPDF2>=3.0.1", "python_Levenshtein>=0.21.0", "pyvis>=0.3.2", "scholarly>=1.7.11", "scidownl>=1.0.2", "setuptools>=67.7.2", "six>=1.16.0", "spacy>=3.2.0", "yake>=0.4.8"],
     extras_require={
         "dev": ["pytest>=7.0", "twine>=4.0.2"],
     },
