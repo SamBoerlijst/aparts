9216c68f70588d649ace476060d71eca7bf173b0
Sam Boerlijst <34442338+SamBoerlijst@users.noreply.github.com>
2023-08-15 19:02:27 +0200

    Add files via upload
    Integrated deduplication workflow to merge synonyms and other related tags by similarity. Additionally included principal component analysis (PCA) to identify the most important tags in the dataset.

---
+++ app/aparts/src/deduplication.py
@@ -0,0 +1,389 @@
+from itertools import combinations
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+from sklearn.cluster import AgglomerativeClustering
+from sklearn.decomposition import PCA
+from sklearn.metrics import pairwise_distances
+from sklearn.preprocessing import StandardScaler
+
+from aparts.src.subsampling import (assign_group, generate_binary_item_matrix,
+                                    transform_dataframe)
+
+
+def group_tags_by_dissimilarity(dissimilarity_matrix: np.ndarray, tag_names: list, threshold: float = 0.5, print_output: bool = False) -> list:
+    clustering = AgglomerativeClustering(
+        n_clusters=None, linkage='average', distance_threshold=threshold, metric='precomputed')
+    clusters = clustering.fit_predict(dissimilarity_matrix)
+
+    grouped_columns = {}
+    for col_idx, cluster_id in enumerate(clusters):
+        if cluster_id not in grouped_columns:
+            grouped_columns[cluster_id] = []
+        grouped_columns[cluster_id].append(col_idx)
+
+    # Filter clusters to include only those with 2 or more items
+    filtered_groups = [
+        group for group in grouped_columns.values() if len(group) >= 2]
+
+    # Substitute indices with tag names
+    nnamed_groups = [[tag_names[col_idx] for col_idx in group]
+                     for group in filtered_groups]
+
+    return nnamed_groups
+
+
+def generate_tag_dissimilarity(dataframe: pd.DataFrame) -> np.ndarray:
+    binary_dataframe = dataframe.astype(int)
+    matrix = binary_dataframe.values
+    dissimilarity_matrix = pairwise_distances(matrix.T, metric='braycurtis')
+    return dissimilarity_matrix
+
+
+def deduplicate_tag_conjugations(word_list: list, method: str = "", deleted_pairs: list = None) -> list:
+    """
+    Identify and delete duplicate tags based on their stem from a (nested) list.
+
+    This function takes a nested list of words and performs deduplication by identifying duplicate tags based on their stem.
+    It also provides an option to output the identified duplicates so that their data can be merged in their corresponding matrix.
+
+    Parameters:
+    -----------
+    word_list (list): The nested list of words to be deduplicated.
+
+    method (str): The deduplication method to use. Can be either "deduplicated" or "pairs".
+
+    deleted_pair (list): A list to retain identified duplicate pairs across iterations. Used when method is set to "pairs" and the input is a nested list.
+
+    Returns:
+    --------
+    list: A deduplicated version of the input nested list. If method is "deduplicated", 
+          this list contains the deduplicated words and sublists. If method is "pairs", 
+          this list contains pairs of duplicate words and their corresponding stems.
+    """
+    if deleted_pairs is None:
+        deleted_pairs = []
+
+    sorted_words = sorted(word_list, key=len, reverse=True)
+    processed_prefixes = set()
+    deduplicated = []
+
+    for word in sorted_words:
+        pair = ()
+        if isinstance(word, list):
+            deduplicated_sublist = deduplicate_tag_conjugations(
+                word, method, deleted_pairs)
+            deduplicated.append(deduplicated_sublist)
+        elif isinstance(word, str):
+            is_duplicate = any(word.startswith(
+                existing_prefix[:len(word)]) for existing_prefix in processed_prefixes)
+            if is_duplicate:
+                pair = (word, next(existing_prefix for existing_prefix in processed_prefixes if word.startswith(
+                    existing_prefix[:len(word)])))
+                deleted_pairs.append(pair)
+            else:
+                deduplicated.append(word)
+                processed_prefixes.add(word)
+
+    return deleted_pairs if method == "pairs" else deduplicated
+
+
+def deduplicate_dataframe(dataframe: pd.DataFrame, pairs_list: list, mode: str = "strict") -> pd.DataFrame:
+    """
+    Deduplicate a DataFrame based on the provided pairs and mode.
+
+    Parameters:
+    -----------
+    dataframe (pd.DataFrame): The DataFrame to be deduplicated.
+
+    pairs_list (list): A list of pairs, where each pair is a tuple containing source and sink column names.
+
+    mode (str): The deduplication mode. Can be 'strict' or 'lenient'. In strict mode the contents of the source column are copied to the sink column. 
+    The source column is subsequently deleted. In lenient mode, the source column is deleted if its contents are identical to the sink column.
+
+    Returns:
+    --------
+    pd.DataFrame: The deduplicated DataFrame.
+    """
+    deduplicated_dataframe = dataframe.copy()
+
+    for pair in pairs_list:
+        source, sink = pair
+
+        if source not in deduplicated_dataframe.columns or sink not in deduplicated_dataframe.columns:
+            continue
+
+        sourcedata = deduplicated_dataframe[source]
+        sinkdata = deduplicated_dataframe[sink]
+
+        if mode == "strict":
+            for i in range(len(sourcedata)):
+                if sourcedata[i] > 0:
+                    sinkdata[i] = sourcedata[i]
+            deduplicated_dataframe[sink] = sinkdata
+            deduplicated_dataframe.drop(columns=[source], inplace=True)
+
+        if mode == "lenient":
+            if sourcedata.equals(sinkdata):
+                deduplicated_dataframe.drop(columns=[source], inplace=True)
+
+    return deduplicated_dataframe
+
+
+def merge_similar_tags_from_dataframe(input_file: str, output: str, variables: str, id: str, tag_length: int, threshold: float = 0.6, manual: bool = False, show_output: bool = False):
+    """
+    Merges similar tags in a DataFrame based on tag similarity using various deduplication methods.
+
+    Parameters:
+    -----------
+    input_file (str): Path to the CSV file containing the data.
+
+    output (str): Filename for an output csv. Data is not saved, but only returned, if left blank.
+
+    variables (str): Comma-separated list of tag/column names.
+
+    id (str): Column name containing unique titles/identifiers.
+
+    tag_length (int): Length of tag n-grams for similarity comparison.
+
+    threshold (float, optional): Similarity threshold for grouping tags. Default is 0.6.
+
+    manual (bool): include manual check of potential duplicates by y/n prompt per pair to either merge or discard ('q' to escape). Default is False.
+
+    show_output (bool): Whether to display intermediate output. Default is False.
+
+    Returns:
+    --------
+    pd.DataFrame: DataFrame with similar tags merged using the specified deduplication methods.
+    """
+    def generate_tuple_combinations(nested_list: str) -> list:
+        """
+        Generate all possible combinations of tag pairs from a nested list of tags.
+        """
+        combination = []
+
+        for item in nested_list:
+            product = []
+            product = list(combinations(item, 2))
+            combination.extend(product)
+
+        return combination
+
+    def calculate_tag_similarity(Dataframe: pd.DataFrame, method: str, treshold: float) -> tuple[list, np.ndarray]:
+        """
+        Calculate tag similarity and deduplicate similar tags in the DataFrame.
+        """
+        tag_dissimilarity = generate_tag_dissimilarity(Dataframe)
+        tag_names = Dataframe.columns
+        grouped_tags = group_tags_by_dissimilarity(
+            tag_dissimilarity, tag_names, treshold)
+        deduplicated_tags = deduplicate_tag_conjugations(grouped_tags, method)
+
+        return deduplicated_tags, tag_dissimilarity
+
+    def manual_deduplication(tuples, dataframe):
+        """
+        Suggest potential duplicates and merge or discard based on user input"""
+        deduplicated_dataframe = dataframe.copy()
+
+        for source, sink in tuples:
+            if source in deduplicated_dataframe.columns and sink in deduplicated_dataframe.columns:
+                sourcedata = deduplicated_dataframe[source]
+                sinkdata = deduplicated_dataframe[sink]
+                answer = input(f"Merge {source} to {sink}? (y/n): ").lower()
+                if answer == "y":
+                    print(f"Merged {source} to {sink}")
+                    sinkdata = sinkdata.add(sourcedata, fill_value=0)
+                    deduplicated_dataframe[sink] = sinkdata
+                    deduplicated_dataframe.drop(columns=[source], inplace=True)
+                elif answer == "n":
+                    break
+                elif answer == "q":
+                    return deduplicated_dataframe
+                else:
+                    print("Input invalid. Please answer with 'y' or 'n'")
+
+        return deduplicated_dataframe
+
+    matrix = generate_binary_item_matrix(
+        input_file, variables, id, tag_length)[0]
+    matrix = drop_0_columns(matrix)
+    deduplicated_tags = calculate_tag_similarity(matrix, "pairs", threshold)[0]
+
+    matrix_deduplicated = deduplicate_dataframe(
+        matrix, deduplicated_tags, "strict")
+    deduplicated_tags_control = calculate_tag_similarity(
+        matrix_deduplicated, "", threshold)[0]
+    potential_duplicates = generate_tuple_combinations(
+        deduplicated_tags_control)
+
+    matrix_deduplicated_lenient = deduplicate_dataframe(
+        matrix, potential_duplicates, "lenient")
+    deduplicated_tags_remaining = calculate_tag_similarity(
+        matrix_deduplicated_lenient, "", threshold)[0]
+    remaining_duplicates = [
+        item for item in deduplicated_tags_remaining if len(item) > 1]
+    remaining_duplicate_tuple = generate_tuple_combinations(
+        remaining_duplicates)
+
+    if manual:
+        matrix_deduplicated_manual = manual_deduplication(
+            remaining_duplicate_tuple, matrix_deduplicated_lenient)
+    else:
+        matrix_deduplicated_manual = matrix_deduplicated_lenient
+
+    if show_output:
+        deduplicated_tags_remaining, tag_dissimilarity3 = calculate_tag_similarity(
+            matrix_deduplicated_manual, "", threshold)
+        plt.figure()
+        plt.imshow(tag_dissimilarity3)
+
+    if output:
+        matrix_deduplicated_manual.to_csv(output, sep=',')
+
+    return matrix_deduplicated_manual
+
+
+def count_tag_occurrence(dataframe: pd.DataFrame) -> np.array:
+    # Assuming the binary columns start from column index 1
+    binary_columns = dataframe.columns[1:]
+
+    counts = []
+    for column in binary_columns:
+        count = dataframe[column].sum()
+        if count > 0:
+            counts.append((column, count))
+    counts.sort(key=lambda x: x[1], reverse=True)
+    return counts
+
+
+def drop_0_columns(dataframe: pd.DataFrame) -> pd.DataFrame:
+    """
+    Delete any columns without observations from the given dataframe.
+    """
+    zero_columns = dataframe.columns[(dataframe == 0).all()]
+    dataframe.drop(zero_columns, axis=1, inplace=True)
+    return dataframe
+
+
+def drop_unique_columns(Dataframe: pd.DataFrame) -> pd.DataFrame:
+    """
+    Drop all single observation columns from a dataframe    
+    """
+    filtered_dataframe = Dataframe.copy()
+    counts = count_tag_occurrence(Dataframe)
+    for item, count in counts:
+        if count == 1:
+            filtered_dataframe.drop(columns=[item], inplace=True)
+    return filtered_dataframe
+
+
+def plot_pca_tags(data: pd.DataFrame, n_components_for_variance: int = 0, show_plots: str = ""):
+    column_names = list(data.columns)
+    # You need to define assign_group function
+    groups = assign_group(data, column_names)
+    group_list = set(groups)
+    # You need to define transform_dataframe function
+    X, y, targets, features = transform_dataframe(data, groups, group_list)
+
+    scaler = StandardScaler()
+    X_scaled = scaler.fit_transform(X)
+    pca = PCA()
+    pca.fit_transform(X_scaled)
+
+    explained_variance_ratio = pca.explained_variance_ratio_
+    cumulative_variance = np.cumsum(explained_variance_ratio)
+    num_components_for_95_variance = np.argmax(cumulative_variance >= 0.95) + 1
+
+    def plots(show_plots):
+        plot_mapping = {
+            "scree": {"function": plot_scree},
+            "saturation": {"function": plot_saturation},
+            "loading": {"function": plot_loading}
+        }
+
+        selected_plots = show_plots.split("and")
+        num_subplots = len(selected_plots)
+
+        fig, axes = plt.subplots(1, num_subplots, figsize=(18, 5))
+        if num_subplots == 1:
+            axes = [axes]
+
+        for i, selected_plot in enumerate(selected_plots):
+            plot_info = plot_mapping[selected_plot.strip()]
+            plot_function = plot_info["function"]
+
+            if num_subplots == 1:
+                subplot_index = 0
+            else:
+                subplot_index = i
+
+            ax = axes[subplot_index]
+            plot_function(ax)
+
+        plt.tight_layout()
+        plt.show()
+
+    def plot_scree(ax):
+        ax.plot(range(1, len(explained_variance_ratio) + 1),
+                explained_variance_ratio, marker='o')
+        ax.set_xlabel('Principal Component')
+        ax.set_ylabel('Explained Variance Ratio')
+        ax.set_title('Scree Plot')
+
+    def plot_saturation(ax):
+        ax.plot(range(1, len(cumulative_variance) + 1),
+                cumulative_variance, marker='o', color='r')
+        ax.set_xlabel('Number of Principal Components')
+        ax.set_ylabel('Cumulative Explained Variance')
+        ax.set_title('Saturation Plot')
+        ax.text(y=0.05, x=0.5,
+                s=f"Number of components needed for 95% explained variance: {num_components_for_95_variance}")
+
+    def plot_loading(ax):
+        loading_matrix = pca.components_.T * np.sqrt(pca.explained_variance_)
+        for i, feature in enumerate(features):
+            ax.arrow(0, 0, loading_matrix[i, 0],
+                     loading_matrix[i, 1], color='r', alpha=0.5)
+            ax.text(loading_matrix[i, 0],
+                    loading_matrix[i, 1], feature, color='g')
+        ax.set_xlim(-1, 1)
+        ax.set_ylim(-1, 1)
+        ax.set_xlabel('PC1 Loading')
+        ax.set_ylabel('PC2 Loading')
+        ax.set_title('Loading Plot')
+
+    def find_contributing_tags(components: int):
+        loading_matrix = pca.components_.T * np.sqrt(pca.explained_variance_)
+        main_tags = []
+        for loading in loading_matrix:
+            indices_sorted_by_loading = np.argsort(np.abs(loading))[::-1]
+            main_tags.append(features[indices_sorted_by_loading[0]])
+        main_tags = sorted(set(main_tags[:components]))
+        main_tags_str = ', '.join(main_tags)
+        print(
+            f'Main contributing tags for {components}% explained variance: {main_tags_str}')
+
+    if "all" in show_plots:
+        show_plots = "loading and scree and saturation"
+
+    if n_components_for_variance > 0:
+        find_contributing_tags(n_components_for_variance)
+
+    plots(show_plots)
+    plt.show()
+
+    return
+
+
+def main(input_file: str, output: str, variables: str, id: str, tag_length: int, n_components_for_variance: int, show_plots: str):
+    Dataframe_merged = merge_similar_tags_from_dataframe(
+        input_file, output, variables, id, tag_length)
+    Dataframe_filtered = drop_unique_columns(Dataframe_merged)
+    plot_pca_tags(Dataframe_filtered, n_components_for_variance, show_plots)
+
+
+if __name__ == "__main__":
+    main(input_file="C:/NLPvenv/NLP/output/csv/savedrecs_lianas.csv", output="C:/NLPvenv/NLP/output/csv/lianas_deduplicated.csv",
+         variables="Keywords", id="Article Title", tag_length=4, n_components_for_variance=10, show_plots="all")
--- app/aparts/src/subsampling.py
+++ app/aparts/src/subsampling.py
@@ -1,10 +1,12 @@
+import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 from numpy.random import default_rng
 from scipy.spatial.distance import braycurtis
-import matplotlib.pyplot as plt
+from sklearn.cluster import KMeans
 
-def generate_binary_matrix(CSV_path: str, y: str, x: str) -> tuple[pd.DataFrame, list]:
+
+def generate_binary_item_matrix(CSV_path: str = "", y: str = "keywords", x: str = "title", keyword_length: int = 3, delimiter: str = ", ") -> tuple[pd.DataFrame, list]:
     """
     Generate a boolean matrix of items by respective tag presence from a csv.
 
@@ -16,30 +18,42 @@ def generate_binary_matrix(CSV_path: str, y: str, x: str) -> tuple[pd.DataFrame,
 
     x (str): Column name of the article identifyers.
 
+    keyword_length (int): Minimum tag length.
+
+    delimiter (str): Separator used to delimit tags.
+
     Returns:
     --------
     binary_matrix_df (numpy.ndarray): boolean matrix of items by tag presence.
 
     rows_list (list): List of article identifyers.
     """
-    def remove_empty_values(x: pd.Series):
-        return x[x != '']
-    dimensions = pd.read_csv(CSV_path)[y].str.replace(",", "").str.split(' ')
-    dimensions = dimensions.astype(str)
-    dimensions = remove_empty_values(dimensions)
+    dimensions = pd.read_csv(CSV_path)[y]
+    dimensions = dimensions.astype(str).sum()
+    dimensions = str(dimensions).split(delimiter)
+    dimensions = [item.lstrip(' ').rstrip(' ') for item in dimensions]
+    dimensions_uniques = sorted(set(dimensions))
+    dimensions_filtered = [
+        item for item in dimensions_uniques if len(item) >= keyword_length]
     rows = pd.read_csv(CSV_path)[x]
-    dimensions_uniques = sorted(
-        list(set([dim for dim_list in dimensions for dim in dim_list])))
     rows_list = rows.tolist()
-    binary_matrix = np.zeros((len(rows), len(dimensions_uniques)), dtype=int)
+    binary_matrix = np.zeros((len(rows), len(dimensions_filtered)), dtype=int)
 
-    for i, item in enumerate(dimensions):
-        for j, dim in enumerate(dimensions_uniques):
-            if dim in item:
+    for i, item in enumerate(rows):
+        for j, dim in enumerate(dimensions_filtered):
+            if dim in dimensions[i]:
                 binary_matrix[i, j] = 1
 
+    # Remove rows with only 0 values
+    non_zero_rows = np.any(binary_matrix, axis=1)
+    binary_matrix = binary_matrix[non_zero_rows]
+    rows_list = [row for i, row in enumerate(rows_list) if non_zero_rows[i]]
+
+    # Update indices
     binary_matrix_df = pd.DataFrame(
-        binary_matrix, columns=dimensions_uniques, index=rows)
+        binary_matrix, columns=dimensions_filtered)
+    binary_matrix_df.index = rows_list
+
     return binary_matrix_df, rows_list
 
 
@@ -102,7 +116,7 @@ def calculate_euclidean_distance_matrix(matrix: np.ndarray) -> np.ndarray:
     return distance_matrix
 
 
-def select_items_by_distance(distance_matrix: np.ndarray, n: int, continuous_dimensions: np.ndarray) -> list:
+def select_items_by_distance(distance_matrix: np.ndarray, n: int, continuous_dimensions: np.ndarray, distance_type: str, start_item: str) -> list:
     """
     Select items based on the highest sum of distances to previously selected items.
 
@@ -114,10 +128,18 @@ def select_items_by_distance(distance_matrix: np.ndarray, n: int, continuous_dim
 
     continuous_dimensions (np.ndarray): Matrix containing the coordinates of the samples.
 
+    distance_type (str): Indicates whether articles should be chosen based on "dissimilarity" or "similarity".
+
+    start_item (str): Indicates whether the first selected item should be the one closest to the "centroid", or in the middle of the largest "cluster".
+
     Returns:
     --------
     selected_items (list): List of selected item indices.
     """
+    type_list = ["dissimilarity", "similarity"]
+    start_list = ["centroid", "cluster"]
+    assert distance_type in type_list, "Please use a distance_type equal to 'dissimilarity' or 'similarity'"
+    assert start_item in start_list, "Please use a start_item equal to 'centroid' or 'cluster'"
     num_items = distance_matrix.shape[0]
     selected_items = []
 
@@ -125,16 +147,41 @@ def select_items_by_distance(distance_matrix: np.ndarray, n: int, continuous_dim
     centroid = np.mean(continuous_dimensions, axis=0)
 
     # Find the index of the item closest to the centroid
-    first_item = np.argmin(np.linalg.norm(continuous_dimensions - centroid, axis=1))
-    selected_items.append(first_item)
+    if start_item == "centroid":
+        first_item = np.argmin(np.linalg.norm(
+            continuous_dimensions - centroid, axis=1))
+        selected_items.append(first_item)
+    elif start_item == "cluster":
+        kmeans = KMeans(n_clusters=5)  # Set the appropriate number of clusters
+        labels = kmeans.fit_predict(continuous_dimensions)
+        cluster_sizes = np.bincount(labels)
+        largest_cluster_index = np.argmax(cluster_sizes)
+        largest_cluster_items = np.where(labels == largest_cluster_index)[0]
+        largest_cluster_centroid = np.mean(
+            continuous_dimensions[largest_cluster_items], axis=0)
+        starting_item = largest_cluster_items[np.argmin(np.linalg.norm(
+            continuous_dimensions[largest_cluster_items] - largest_cluster_centroid, axis=1))]
+        selected_items.append(starting_item)
 
     for _ in range(n - 1):
-        item_sums = np.sum(distance_matrix[selected_items][:, np.newaxis], axis=0).ravel()
-        item_sums[selected_items] = -np.inf  # Set previously selected items' sums to -inf
-        next_item = np.argmax(item_sums)
+        item_sums = np.sum(
+            distance_matrix[selected_items][:, np.newaxis], axis=0).ravel()
+        # Set previously selected items' sums to -inf
+        item_sums[selected_items] = -np.inf
+
+        masked_sums = np.ma.array(item_sums, mask=False)
+        # Mask previously selected items
+        masked_sums[selected_items] = np.ma.masked
+
+        if distance_type == "dissimilarity":
+            next_item = np.ma.argmax(masked_sums)
+        elif distance_type == "similarity":
+            next_item = np.ma.argmin(masked_sums)
+
         selected_items.append(next_item)
 
     return selected_items
+
 
 def get_selected_coordinates(selected_items: list, distance_matrix: np.ndarray) -> np.ndarray:
     """
@@ -153,6 +200,7 @@ def get_selected_coordinates(selected_items: list, distance_matrix: np.ndarray) 
     selected_coordinates = distance_matrix[selected_items]
 
     return selected_coordinates
+
 
 def get_sample_id(indices: list, id_list: list) -> list:
     """
@@ -176,7 +224,7 @@ def get_sample_id(indices: list, id_list: list) -> list:
     return names
 
 
-def subsample_from_csv(CSV_path: str = "", y: str = "keywords", x: str = "title", n: int = 100):
+def subsample_from_csv(CSV_path: str = "", y: str = "keywords", x: str = "title", n: int = 100, save_plot: bool = False, distance_type: str = "dissimilarity", start_item: str = "centroid"):
     """
     Subsamples n papers by 3D bray-curtis dissimilarity based on their tags, and evenly selecting items across the euclidean space.
 
@@ -190,21 +238,47 @@ def subsample_from_csv(CSV_path: str = "", y: str = "keywords", x: str = "title"
 
     n (int): The number of articles to select.
 
+    save_plot (bool): Indicates whether the 3D plot of corpus and selection should be saved to the current folder as png.
+
+    distance_type (str): Indicates whether articles should be chosen based on "dissimilarity" or "similarity".
+
+    start_item (str): Indicates whether the first selected item should be the one closest to the "centroid", or in the middle of the largest "cluster".
+
     Returns:
     --------
     titles (list): List of identifyers for the selected items.
     """
-    matrix, id_list = generate_binary_matrix(CSV_path, y, x)
+    matrix, id_list = generate_binary_item_matrix(CSV_path, y, x)
     dissimilarity_array = generate_bray_curtis_dissimilarity(matrix)
-    eucllidean_matrix = calculate_euclidean_distance_matrix(dissimilarity_array)
-    indices = select_items_by_distance(eucllidean_matrix, n, dissimilarity_array)
+    eucllidean_matrix = calculate_euclidean_distance_matrix(
+        dissimilarity_array)
+    indices = select_items_by_distance(
+        eucllidean_matrix, n, dissimilarity_array, distance_type, start_item)
     selected_array = get_selected_coordinates(indices, dissimilarity_array)
-    plot_array(dissimilarity_array, selected_array, n, False)
+    plot_array(dissimilarity_array, selected_array, n, save_plot)
     titles = get_sample_id(indices, id_list)
     return titles
 
-def plot_array(totalset:np.array, selected_set:np.array, n:int, save_plot:bool)->None:
-    def array_to_3d_array(array:np.array)-> tuple[np.ndarray, np.ndarray, np.ndarray]:
+
+def plot_array(total_set: np.array, selected_set: np.array, n: int, save_plot: bool) -> None:
+    """
+    Generate a 3D plot of the selected articles superimposed over the corpus.
+
+    Parameters:
+    -----------
+    total_set (np.array): 3D dissimilarity matrix of continuous values for the entire corpus.
+
+    selected_set (np.array): 3D dissimilarity matrix of continuous values for the selected articles.
+
+    n (int): The number of selected_articles (to include in filename).
+
+    save_plot (bool): Indicates whether the 3D plot of corpus and selection should be saved to the current folder as png.
+
+    Returns:
+    --------
+    None
+    """
+    def array_to_3d_array(array: np.array) -> tuple[np.ndarray, np.ndarray, np.ndarray]:
         x = []
         y = []
         z = []
@@ -217,18 +291,37 @@ def plot_array(totalset:np.array, selected_set:np.array, n:int, save_plot:bool)-
         y = np.asarray(y)
         z = np.asarray(z)
         return x, y, z
-    a,b,c = array_to_3d_array(totalset)
-    d,e,f = array_to_3d_array(selected_set)
+    a, b, c = array_to_3d_array(total_set)
+    d, e, f = array_to_3d_array(selected_set)
     fig = plt.figure()
     ax = fig.add_subplot(111, projection='3d')
-    ax.scatter(a, b, -c, zdir='z', c= 'red')
-    ax.scatter(d, e, -f, zdir='z', c= 'blue')
+    ax.scatter(a, b, -c, zdir='z', c='red', alpha=0.3)
+    ax.scatter(d, e, -f, zdir='z', c='black')
     if save_plot == True:
         plt.savefig(f"corpus and selection n{n}.png")
     return
 
+
+def transform_dataframe(df: pd.DataFrame, target: list, target_list: list) -> tuple[np.ndarray, list, list, list]:
+    data = df.values
+    target_names = target_list
+    feature_names = df.columns.tolist()
+
+    return data, target, target_names, feature_names
+
+
+def assign_group(binary_dataframe, item_list):
+    assigned_items = []
+    for index, row in binary_dataframe.iterrows():
+        for item in item_list:
+            if row[item] == 1:
+                assigned_items.append(item)
+                break
+    return assigned_items
+
+
 if __name__ == "__main__":
-    titles = subsample_from_csv(
-        CSV_path="C:/NLPvenv/NLP/output/csv/total.csv", n=30)
+    titles = subsample_from_csv(CSV_path="C:/NLPvenv/NLP/output/csv/savedrecs_lianas.csv",
+                                y="Keywords", x="Article Title", n=40, distance_type="dissimilarity")
     for item in titles:
         print(item)
